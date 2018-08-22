020bd5199a50edea52eaef77d20c70bb1dc11abe
Jānis Iesalnieks <iesalnieks.juniors@gmail.com>
2018-08-22 10:46:44 +0300

    Comments

--- routes/media.js
+++ routes/media.js
@@ -4,6 +4,7 @@ import { uploadImage, addPost, getAllPosts, getPostById } from '../controllers/m
 import asyncMiddleware from '../middlewares/asyncMiddleware';
 import diskStorageSingle from '../middlewares/diskStorage';
 import authenticate from '../middlewares/authenticate';
+import { getPostComments, addPostComments } from '../controllers/commentController';
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.post('/content/image', authenticate, diskStorageSingle, asyncMiddleware(u
 router.post('/media', authenticate, asyncMiddleware(addPost));
 router.get('/media', authenticate, asyncMiddleware(getAllPosts));
 router.get('/media/:id', authenticate, asyncMiddleware(getPostById));
+router.get('/:mediaId/comments', authenticate, asyncMiddleware(getPostComments));
+router.post('/:mediaId/comments', authenticate, asyncMiddleware(addPostComments));
 
 export default router;
